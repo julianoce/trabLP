5de29abcd39b4ec84dc99fa0c53086fe3ebcf75f
julianoce <julianoce@hotmail.com>
2018-05-16 21:03:37 -0300

    apenas a entrada do trabalho rsrs

---
+++ trabalho.rkt
@@ -0,0 +1,8 @@
+#lang racket
+
+(printf "Escreva seu grafo aqui: (vertice,aresta,vertice)\n")
+(define a(read))
+(printf"Seu grafo eh:~a\n" a)
+(printf "Escreva sua PDL aqui:\n")
+(define b(read))
+(printf"Sua pdl eh:~a\n" b)
